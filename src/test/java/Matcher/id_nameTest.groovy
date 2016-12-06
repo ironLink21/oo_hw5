ba6d02961b18cfecf22c8728b777587c39a305e0
Seth Bertlshofer <celestials2013@gmail.com>
2016-12-05 23:52:21 -0700

    created test files

---
+++ src/test/java/Matcher/id_nameTest.groovy
@@ -0,0 +1,22 @@
+package Matcher
+
+/**
+ * Created by seth on 12/5/16.
+ */
+class id_nameTest extends GroovyTestCase {
+    void testOperation1() {
+
+    }
+
+    void testOperation2() {
+
+    }
+
+    void testOperation3() {
+
+    }
+
+    void testSetup() {
+
+    }
+}
---
+++ src/test/java/Matcher/mother_birthTest.groovy
@@ -0,0 +1,18 @@
+package Matcher
+
+/**
+ * Created by seth on 12/5/16.
+ */
+class mother_birthTest extends GroovyTestCase {
+    void testOperation1() {
+
+    }
+
+    void testOperation2() {
+
+    }
+
+    void testOperation3() {
+
+    }
+}
